a67966862c4e302a848b16bd3a43d12f38c47f78
joseff01 <joseantonioretanacorrales@gmail.com>
2025-05-14 22:20:16 -0600

    agregar ejemplito 1 y 3

---
+++ ejemplito.py
@@ -0,0 +1,16 @@
+
+class Dog:
+    
+    def __init__(self, color):
+        
+        self.color = color
+
+    def say_color(self):
+        print("my color is: ", self.color)
+        
+
+my_dog = Dog("amarillo")
+
+my_dog.say_color()
+
+
